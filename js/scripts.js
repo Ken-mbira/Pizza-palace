932c2ba3dee901c85a5d86b9f26e1dd42e680197
ken <ken.mbira@student.moringaschool.com>
2021-07-31 21:42:25 +0300

    modify the navigation bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $("button#topage2").click(function() {
+    $(".topage2").click(function() {
         $("#page1").hide();
         $("#page2").show();
     })
-    $("button#topage1").click(function() {
+    $(".topage1").click(function() {
         $("#page2").hide();
         $("#page1").show();
     })
