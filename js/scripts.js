1da1c435e463d6585ed2db7d0887b2103291966f
ken <ken.mbira@student.moringaschool.com>
2021-08-01 18:01:19 +0300

    add a form to input user preference

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,19 @@ $(document).ready(function() {
         $("#page2").hide();
         $("#page1").show();
     })
-});
+    $(".dropdown-content").hover(function() {
+        $("#pizza-choice").show();
+    }, function() {
+        $("#pizza-choice").hover(function() {
+            $("#pizza-choice").show();
+        }, function() {
+            $("#pizza-choice").hide();
+        });
+    })
+    $("button.add-item").click(function() {
+        $("#page2").hide();
+        $("#page3").show();
+    })
+
+
+})
